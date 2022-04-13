5baf7e798f38277fb2e4548638310c4f2a59cfc4
Hugo Serra <hugo.serra8@gmail.com>
2022-04-13 20:07:29 +0100

    🐛 Update Module exports

--- next.config.js
+++ next.config.js
@@ -2,5 +2,6 @@ const withNextra = require('nextra')({
     theme: 'nextra-theme-docs',
     themeConfig: './theme.config.js',
     // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
+    assetPrefix: './',
 })
 module.exports = withNextra()
