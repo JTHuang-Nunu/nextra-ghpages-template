746dde4fe0a4087bc00862b45be20c8ce607dd93
Hugo Serra <hugo.serra8@gmail.com>
2022-04-13 21:10:24 +0100

    🐛 Fix GH Pages necessary config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@ const withNextra = require('nextra')({
     theme: 'nextra-theme-docs',
     themeConfig: './theme.config.js',
     // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
-    assetPrefix: 'https://hugo-serra.github.io/nextra-ghpages-template',
 })
-module.exports = withNextra()
+
+let nextraConfig = withNextra()
+nextraConfig.assetPrefix = './'
+module.exports = nextraConfig
